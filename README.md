7ae3f24f64280e2733e27ba2c272a1c285362201
MarioHeleno <mmariohelenosilva@gmail.com>
2021-02-12 11:44:39 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# learningangular-zfqmur
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/learningangular-zfqmur)
---
+++ src/app/products.ts
@@ -0,0 +1,26 @@
+export const products = [
+  {
+    id: 1,
+    name: "Phone XL",
+    price: 799,
+    description: "Um celular com uma tela enorme"
+  },
+  {
+    id: 2,
+    name: "Phone Mini",
+    price: 699,
+    description: "Um celular com uma das melhores câmeras"
+  },
+  {
+    id: 3,
+    name: "Phone Standard",
+    price: 299,
+    description: ""
+  }
+];
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
